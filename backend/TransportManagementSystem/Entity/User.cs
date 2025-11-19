8e005daf03d17f3ea8265c0d4d973de488e98bc8
AzureAD\me002 <bacemadhimi11@gmail.com>
2025-11-19 15:54:03 +0100

    Ajouter la classe user

---
+++ backend/TransportManagementSystem/Entity/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TransportManagementSystem.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string? ProfileImage { get; set; }
+
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+
+    }
+}
