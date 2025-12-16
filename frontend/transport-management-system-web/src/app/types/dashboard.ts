4e8982d17e936ad598f5cadbcc6e6954c62d3d39
AzureAD\me002 <bacemadhimi11@gmail.com>
2025-12-16 13:48:01 +0100

    DashBoard :Nombre total des utilisateurs,chauffeurs,camions

---
+++ frontend/transport-management-system-web/src/app/types/dashboard.ts
@@ -0,0 +1,10 @@
+export interface IDashboard{
+    userCount: number
+    driverCount: number
+    truckCount: number
+}
+export interface IDepartmentData{
+    
+     name: String,
+     DriverCount: number 
+    }
