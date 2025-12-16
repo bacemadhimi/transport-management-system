13f6af03c46bacb452f07ceedcca75fff48b140c
AzureAD\me002 <bacemadhimi11@gmail.com>
2025-12-16 15:26:19 +0100

    ajouter Identity

--- backend/TransportManagementSystem/Entity/Customer.cs
+++ backend/TransportManagementSystem/Entity/Customer.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TransportManagementSystem.Entity
 {
     public class Customer
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
