5edf85dd8fa0cc25bf64d44168ea8feaae8ccb7b
Zeineb Bessaidia <zeineb.bessaidia@gmail.com>
2025-12-19 13:25:38 +0100

    add country code for phone number for customer

--- backend/TransportManagementSystem/Models/CustomerDto.cs
+++ backend/TransportManagementSystem/Models/CustomerDto.cs
@@ -4,6 +4,7 @@
     {
         public required string Name { get; set; }
         public required string Phone { get; set; }
+        public required string phoneCountry { get; set; }
         virtual public required string Email { get; set; }
         public required string Adress { get; set; }
     }
