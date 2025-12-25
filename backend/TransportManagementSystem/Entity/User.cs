43588bd0d6df7e138ccfa54d5e956da6ca0e5215
Zeineb Bessaidia <zeineb.bessaidia@gmail.com>
2025-12-25 14:25:51 +0100

    change password not required for user

--- backend/TransportManagementSystem/Entity/User.cs
+++ backend/TransportManagementSystem/Entity/User.cs
@@ -7,7 +7,7 @@ public class User
 {
     public int Id { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
 
     [Required]
     [ForeignKey("Role")]
