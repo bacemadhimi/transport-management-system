1bdccd216b5e8e4b37f19710ce3c4aa787cfafd9
SHILI <kamelshili.contact@gmail.com>
2026-01-12 10:57:00 +0100

    Update Customern

--- frontend/transport-management-system-web/src/app/types/customer.ts
+++ frontend/transport-management-system-web/src/app/types/customer.ts
@@ -6,9 +6,7 @@ export interface ICustomer {
   email: string;
   adress: string;
   matricule: string;
-   familleProduct: string;
   gouvernorat: string;
   contact: string;
   zone: string; 
-  typeAdress: string;
 }
